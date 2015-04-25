85fcca704d5c89c08383bb3a231b13794ef6cf79
Jonas Ostir <Jonas.Ostir@gmail.com>
2015-04-25 10:29:00 +0200

    Created new project with my name in it as requested and pasted the 'old' code inside it

---
+++ movieApp_JonasOstir/www/js/app.js
@@ -0,0 +1,73 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var app = angular.module('movieApp_JonasOstir', [
+	'ionic',
+	'ngResource',
+	'moviesService',
+	'moviesControllers'
+]);
+
+app.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+		// for form inputs)
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			// org.apache.cordova.statusbar required
+			StatusBar.styleDefault();
+		}
+	});
+})
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise('/movies');
+
+	$stateProvider.state('app', {
+		abstract: true,
+		templateUrl: 'templates/main.html',
+	});
+
+	$stateProvider.state('app.movies', {
+		abstract: true,
+		url: '/movies',
+		views: {
+			movies: {
+				template: '<ion-nav-view></ion-nav-view>'
+			}
+		}
+	});
+
+	$stateProvider.state('app.movies.index', {
+		url: '',
+		templateUrl: 'templates/movies.html',
+		controller: 'moviesController',
+	});
+
+	$stateProvider.state('app.movies.detail', {
+		url: '/:movie',
+		templateUrl: 'templates/movie.html',
+		controller: 'movieDetailController',
+		resolve: {
+			movie: function($stateParams, Movies) {
+				var m = Movies.get({
+					id: $stateParams.movie
+				});
+				return m;
+			}
+		}
+	});
+
+	$stateProvider.state('app.help', {
+		url: '/help',
+		views: {
+			help: {
+				templateUrl: 'templates/help.html'
+			}
+		}
+	});
+});
